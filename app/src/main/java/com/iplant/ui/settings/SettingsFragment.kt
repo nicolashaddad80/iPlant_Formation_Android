e26a9e7851925cb8beed4cee6cb7f708b1ec60d8
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-17 17:02:26 +0200

    added correction from teancher:API

--- app/src/main/java/com/iplant/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/iplant/ui/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.iplant.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,21 +16,30 @@ import com.iplant.databinding.FragmentSettingsBinding
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
-
+    private  val mesPrefs = activity?.getSharedPreferences("PREFS",Context.MODE_PRIVATE)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
 
         val binding: FragmentSettingsBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_settings, container, false
         )
+        val nightModeEnabled = mesPrefs?.getBoolean("Night_Mode", false)
+        binding.switch1.isChecked = nightModeEnabled?:false
 
         binding.switch1.setOnCheckedChangeListener { _, isChecked: Boolean->
-             AppCompatDelegate.setDefaultNightMode(if (isChecked) AppCompatDelegate.MODE_NIGHT_YES
-             else AppCompatDelegate.MODE_NIGHT_NO)
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) {
+                    mesPrefs?.edit()?.putBoolean("Night_Mode", true)?.apply()
+                    AppCompatDelegate.MODE_NIGHT_YES
+                } else {
+                    mesPrefs?.edit()?.putBoolean("Night_Mode", false)?.apply()
+                    AppCompatDelegate.MODE_NIGHT_NO
+                })
+
         }
         return binding.root
     }
